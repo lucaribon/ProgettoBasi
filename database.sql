20294f2cd98473aa9ca6bffe1ca0ea81f73ec37f
bazz333 <matteobazzan333@gmail.com>
2024-07-02 11:06:29 +0200

    PUSHO FORTE
    Co-authored-by: Luca Ribon <lucaribon@users.noreply.github.com>

--- database.sql
+++ database.sql
@@ -18,16 +18,18 @@ DROP TYPE IF EXISTS TIPOTRAZIONE;
 DROP TYPE IF EXISTS TIPOALIMENTAZIONE;
 DROP TYPE IF EXISTS TIPOCAMBIO;
 
+-- CREAZIONE TIPI ENUMERATORI
 CREATE TYPE TIPOTRAZIONE as ENUM ('Anteriore', 'Integrale', 'Posteriore');
 CREATE TYPE TIPOALIMENTAZIONE as ENUM ('Benzina', 'Diesel', 'Etanolo', 'Elettrico', 'Elettrico/Benina','Elettrico/Diesel', 'GPL', 'Metano');
 CREATE TYPE TIPOCAMBIO as ENUM ('Automatico', 'Manuale', 'Semiautomatico');
 
+-- CREAZIONE TABELLE
 CREATE TABLE Utente(
     Email VARCHAR(64) PRIMARY KEY,
     Pass VARCHAR(64) NOT NULL,
     Telefono VARCHAR(10) NOT NULL,
     NumeroAnnunci INT NOT NULL,
-    CHECK (NumeroAnnunci >= 0)
+    CHECK (NumeroAnnunci == 0) -- guardo che sia zero perchè è il nuovo utente
 );
 
 CREATE TABLE Recensione(
@@ -60,10 +62,11 @@ CREATE TABLE Impresa(
 );
 
 CREATE TABLE Privato(
-    EmailUtente VARCHAR(64),
-    NomeCognome VARCHAR(64),
-    DataNascita DATE
-)
+    EmailUtente VARCHAR(64) NOT NULL PRIMARY KEY,
+    NomeCognome VARCHAR(64) NOT NULL,
+    DataNascita DATE,
+    FOREIGN KEY (EmailUtente) REFERENCES Utente(Email)
+);
 
 CREATE TABLE Immagine(
     UrlImmagine VARCHAR(64) NOT NULL PRIMARY KEY,
@@ -140,6 +143,8 @@ CREATE TABLE Veicolo(
     VersioneAuto VARCHAR(64) NOT NULL,
     FOREIGN KEY (MarcaAuto, ModelloAuto, VersioneAuto) REFERENCES Automobile(Marca, Modello, Versione)
 );
+
+-------------------------------------------------------------------------------------------------------
 -- TRIGGER
 
 -- Controllo immagine di copertina
@@ -186,8 +191,127 @@ CREATE OR REPLACE TRIGGER controlloVeicoloNuovoImpresa BEFORE INSERT ON Annuncio
 FOR EACH ROW
 EXECUTE FUNCTION controlloVeicoloNuovo();
 
-
+-------------------------------------------------------------------------------------------------------
 -- INSERIMENTO DATI
 
+-- INSERIMENTO UTENTI
+INSERT INTO Utente(Email, Pass, Telefono, NumeroAnnunci) VALUES 
+('deodatologgia@gmail.com', 'deologgia42', '3367979106', 0), -- privato
+('cirorusso@gmail.com','cirorusso','3274482743', 0), -- privato
+('anitatrevisani@autosrl.com','amolemacchine','3983135981', 0), -- impresa
+('beppefucile@autoclass.it','occhiobalocchio','3331247590', 0), -- impresa
+('lorisgommata@ceccatomotors.it','corrievai', '3245675132', 0); -- impresa
+('wolfgang.chan@supercars.de','mynameiswolfgang','9729421589', 0); -- impresa
 
--- 
+INSERT INTO Privato(EmailUtente, NomeCognome, DataNascita) VALUES
+('deodatologgia@gmail.com','Deodato Loggia', '2000-01-01'),
+('cirorusso@gmail.com', 'Ciro Russo', '1979-04-10');
+
+INSERT INTO Impresa(EmailUtente, CodicePartitaIva, NomeImpresa, SedeComune, SedeCAP) VALUES
+('anitatrevisani@autosrl.com','12345678901','AutoSRL','Palermo','90126'),
+('beppefucile@autoclass.it','98742634145','AutoClass','Padova','35010'),
+('lorisgommata@ceccatomotors.it','45678912301','CeccatoMotors','Vicenza','36100'),
+('wolfgang.chan@supercars.de','78784723144','SuperCars','Berlin','10176');
+
+-- INSERIMENTO LUOGHI
+INSERT INTO Luogo(COMUNE, CAP, PROVINCIA, STATO) VALUES
+('Palermo','90126','PA','IT'),
+('Padova','35010','PD','IT'),
+('Vicenza','36100','VI','IT'),
+('Berlin','10176','BE','DE');
+
+-- INSERIMENTO RECENSIONI
+INSERT INTO Recensione(EmailRecensore, EmailRecensito, Valutazione, Commento) VALUES
+('deodatoLoggia@gmail.com','anitatrevisani@autosrl.com', 8, 'Ottimo venditore, ottima offerta, macchina in perfette condizioni e personale molto cordiale.'),
+('wolfgang.chan@supercars.de','lorisgommata@ceccatomotors.it', 3, 'Als klassischer italienischer Käufer forderte er mich auf, ohne Rechnung zu zahlen, und als ich mich weigerte, geriet er auf dem Parkplatz des Händlers ins Schleudern'),
+('cirorusso@gmail.com', 'beppefucile@autoclass.it', 6, 'Buon venditore, macchina in buone condizioni, ma il prezzo era un po'' troppo alto.'),
+('anitatrevisani@autosrl.com','beppefucile@autoclass.it', 4, 'La macchina non si accende più :(, unlucky'),
+('lorisgommata@ceccatomotors.it','beppefucile@autoclass.it', 1, 'Ho chiesto di pagare all''italiana, ma il venditore ha rifiutato. Inutile dire che me ne sono andato sgommando per la rabbia.'); 
+
+-- INSERIMENTO IMMAGINI
+INSERT INTO Immagine(UrlImmagine, IsCopertina) VALUES
+-- audi a4 2020 business
+('https://www.autosearch.it/img/cirorusso%40gmail%2Ecom/audi-a4-2020-business-1.jpg', TRUE),
+('https://www.autosearch.it/img/cirorusso%40gmail%2Ecom/audi-a4-2020-business-2.jpg', FALSE),
+('https://www.autosearch.it/img/cirorusso%40gmail%2Ecom/audi-a4-2020-business-3.jpg', FALSE),
+('https://www.autosearch.it/img/cirorusso%40gmail%2Ecom/audi-a4-2020-business-4.jpg', FALSE),
+('https://www.autosearch.it/img/cirorusso%40gmail%2Ecom/audi-a4-2020-business-5.jpg', FALSE),
+('https://www.autosearch.it/img/cirorusso%40gmail%2Ecom/audi-a4-2020-business-6.jpg', FALSE),
+('https://www.autosearch.it/img/cirorusso%40gmail%2Ecom/audi-a4-2020-business-7.jpg', FALSE),
+('https://www.autosearch.it/img/cirorusso%40gmail%2Ecom/audi-a4-2020-business-8.jpg', FALSE),
+('https://www.autosearch.it/img/cirorusso%40gmail%2Ecom/audi-a4-2020-business-9.jpg', FALSE),
+('https://www.autosearch.it/img/cirorusso%40gmail%2Ecom/audi-a4-2020-business-10.jpg', FALSE),
+-- audi a4 2020 s-line cirorusso
+('https://www.autosearch.it/img/cirorusso%40gmail%2Ecom/audi-a4-2020-s-line-1.jpg', TRUE),
+('https://www.autosearch.it/img/cirorusso%40gmail%2Ecom/audi-a4-2020-s-line-2.jpg', FALSE),
+('https://www.autosearch.it/img/cirorusso%40gmail%2Ecom/audi-a4-2020-s-line-3.jpg', FALSE),
+('https://www.autosearch.it/img/cirorusso%40gmail%2Ecom/audi-a4-2020-s-line-4.jpg', FALSE),
+('https://www.autosearch.it/img/cirorusso%40gmail%2Ecom/audi-a4-2020-s-line-5.jpg', FALSE),
+('https://www.autosearch.it/img/cirorusso%40gmail%2Ecom/audi-a4-2020-s-line-6.jpg', FALSE),
+('https://www.autosearch.it/img/cirorusso%40gmail%2Ecom/audi-a4-2020-s-line-7.jpg', FALSE),
+-- toyota yaris 2020
+('https://www.autosearch.it/img/anitatrevisani%40autosrl%2Ecom/toyota-yaris-2020-1.jpg', TRUE),
+('https://www.autosearch.it/img/anitatrevisani%40autosrl%2Ecom/toyota-yaris-2020-2.jpg', FALSE),
+('https://www.autosearch.it/img/anitatrevisani%40autosrl%2Ecom/toyota-yaris-2020-3.jpg', FALSE),
+('https://www.autosearch.it/img/anitatrevisani%40autosrl%2Ecom/toyota-yaris-2020-4.jpg', FALSE),
+('https://www.autosearch.it/img/anitatrevisani%40autosrl%2Ecom/toyota-yaris-2020-5.jpg', FALSE),
+('https://www.autosearch.it/img/anitatrevisani%40autosrl%2Ecom/toyota-yaris-2020-6.jpg', FALSE),
+('https://www.autosearch.it/img/anitatrevisani%40autosrl%2Ecom/toyota-yaris-2020-7.jpg', FALSE),
+('https://www.autosearch.it/img/anitatrevisani%40autosrl%2Ecom/toyota-yaris-2020-8.jpg', FALSE),
+('https://www.autosearch.it/img/anitatrevisani%40autosrl%2Ecom/toyota-yaris-2020-9.jpg', FALSE),
+('https://www.autosearch.it/img/anitatrevisani%40autosrl%2Ecom/toyota-yaris-2020-10.jpg', FALSE), 
+-- volkswagen passat 2021
+('https://www.autosearch.it/img/wolfgang%2Echan%40supercars%2Ede/volkswagen-passat-2021-1.jpg', TRUE),
+('https://www.autosearch.it/img/wolfgang%2Echan%40supercars%2Ede/volkswagen-passat-2021-2.jpg', FALSE),
+('https://www.autosearch.it/img/wolfgang%2Echan%40supercars%2Ede/volkswagen-passat-2021-3.jpg', FALSE),
+('https://www.autosearch.it/img/wolfgang%2Echan%40supercars%2Ede/volkswagen-passat-2021-4.jpg', FALSE),
+('https://www.autosearch.it/img/wolfgang%2Echan%40supercars%2Ede/volkswagen-passat-2021-5.jpg', FALSE),
+('https://www.autosearch.it/img/wolfgang%2Echan%40supercars%2Ede/volkswagen-passat-2021-6.jpg', FALSE),
+-- skoda octavia 2021
+('https://www.autosearch.it/img/beppefucile%40autoclass%2Eit/skoda-octavia-2021-1.jpg', TRUE),
+('https://www.autosearch.it/img/beppefucile%40autoclass%2Eit/skoda-octavia-2021-2.jpg', FALSE),
+('https://www.autosearch.it/img/beppefucile%40autoclass%2Eit/skoda-octavia-2021-3.jpg', FALSE),
+('https://www.autosearch.it/img/beppefucile%40autoclass%2Eit/skoda-octavia-2021-4.jpg', FALSE),
+('https://www.autosearch.it/img/beppefucile%40autoclass%2Eit/skoda-octavia-2021-5.jpg', FALSE),
+('https://www.autosearch.it/img/beppefucile%40autoclass%2Eit/skoda-octavia-2021-6.jpg', FALSE),
+('https://www.autosearch.it/img/beppefucile%40autoclass%2Eit/skoda-octavia-2021-7.jpg', FALSE),
+('https://www.autosearch.it/img/beppefucile%40autoclass%2Eit/skoda-octavia-2021-8.jpg', FALSE),
+-- bmw m3 2010 
+('https://www.autosearch.it/img/lorisgommata%40ceccatomotors%2Eit/bmw-m3-2010-1.jpg', TRUE),
+-- audi a4 2006 
+('https://www.autosearch.it/img/beppefucile%40autoclass%2Eit/audi-a4-2006-1.jpg', TRUE),
+('https://www.autosearch.it/img/beppefucile%40autoclass%2Eit/audi-a4-2006-2.jpg', FALSE),
+('https://www.autosearch.it/img/beppefucile%40autoclass%2Eit/audi-a4-2006-3.jpg', FALSE),
+('https://www.autosearch.it/img/beppefucile%40autoclass%2Eit/audi-a4-2006-4.jpg', FALSE),
+
+-- INSERIMENTO MOTORIZZAZIONI
+INSERT INTO Motorizzazione(CodiceMotore, Alimentazione, Potenza, Trazione, Cilindri, Cilindrata) VALUES
+('12345678901', 'Benzina', 150, 'Anteriore', 4, 2000),
+('98742634145', 'Diesel', 120, 'Anteriore', 4, 1600),
+('45678912301', 'Benzina', 500, 'Posteriore', 6, 3000),
+('78784723144', 'Benzina', 300, 'Integrale', 8, 4000),
+('32356479874', 'Elettrico/Benzina', 110, 'Anteriore', 3, 1200);
+
+-- INSERIMENTO AUTO
+INSERT INTO Automobile(Marca, Modello, Versione, Carrozzeria, Cambio, NumPosti, CodiceMotore, ClasseEmissioni) VALUES
+('Audi', 'A4', '2020 Business', 'Station Wagon', 'Automatico', 5, '12345678901', 'Euro 6'),
+('Audi', 'A4', '2020 S-Line', 'Station Wagon', 'Automatico', 5, '12345678901', 'Euro 6'),
+('Toyota', 'Yaris', '2020', 'City Car', 'Automatico', 5, '32356479874', 'Euro 6'),
+('Volkswagen', 'Passat', '2021', 'Station Wagon', 'Automatico', 5, '78784723144', 'Euro 6'),
+('Skoda', 'Octavia', '2021', 'Station Wagon', 'Automatico', 5, '98742634145', 'Euro 6'),
+('BMW', 'M3', '2010', 'Berlina', 'Semiautomatico', 4, '45678912301', 'Euro 4'),
+('Audi', 'A4', '2006', 'Berlina', 'Manuale', 5, '12345678901', 'Euro 3');
+
+-- INSERIMENTO VEICOLI
+INSERT INTO Veicolo(NumeroTelaio, Targa, Colore, MarcaAuto, ModelloAuto, VersioneAuto) VALUES
+('WVWZZZ1JZ3W000000', 'AA000AA', 'Bianco', 'Volkswagen', 'Passat', '2021'),
+('WAUZZZ8KZMA000001', 'AA001AA', 'Nero', 'Audi', 'A4', '2020 Business'),
+('WAUZZZ8KZMA040002', 'AA002AA', 'Bianco', 'Audi', 'A4', '2020 S-Line'),
+('JTDKTUD3000000001', 'AA003AA', 'Blu', 'Toyota', 'Yaris', '2020'),
+('TMBJF9NE3M0000001', 'AA004AA', 'Grigio', 'Skoda', 'Octavia', '2021'),
+('WBSBL92000EW00001', 'AA005AA', 'Nero', 'BMW', 'M3', '2010'),
+('WAUZZZ8E76A000001', 'AA006AA', 'Nero', 'Audi', 'A4', '2006');
+
+-- INSERIMENTO ANNUNCI
+INSERT INTO Annuncio(IdAnnuncio, Descrizione, Prezzo, EmailUtente, NumeroTelaio, CAP, Comune, Chilometraggio, AnnoImmatricolazione) VALUES
+(1, 'Audi A4 2020 Business', 30000.00, '
